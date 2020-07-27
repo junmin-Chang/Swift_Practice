b5d941c264f6698765e04d8597b6c56689ee1d2c
junminChang <chjm219@kookmin.ac.kr>
2020-07-27 16:21:59 +0900

    View_Controller

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by junmin chang on 2020/07/27.
+//  Copyright © 2020 junmin chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    
+    @IBAction func hello(_ sender: Any) {
+        
+        let Alert = UIAlertController(title: "hello", message: "My First App!", preferredStyle: .alert)
+        
+    
+        let Action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        Alert.addAction(Action)
+        
+        present(Alert, animated: true, completion: nil)
+    }
+    
+}
+
